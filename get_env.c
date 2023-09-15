72519b2465f620956141a0c56d62b9c8f14ad691
hannaabdou <hannaabdou743@icloud.com>
2023-09-15 07:19:58 -0700

    a

---
+++ get_env.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+* _getenv - get the value of an environment variable
+* @name: name of the environment variable
+*
+* return: value of the environment variable, or null if it doesnot exist
+*/
+char *_getenv(const char *name)
+{
+char **env;
+size_t name_len = _strlen(name);
+for (env = environment; *env != null; env++)
+{
+if (_strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
+{
+return (&(*env)[name_len + 1]);
+}
+}
+return (null);
+}
