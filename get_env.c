23bbdf77b28852c33326b1ffdf67929fcfda9e2b
hannaabdou <hannaabdou743@icloud.com>
2023-09-15 07:47:51 -0700

    a

--- get_env.c
+++ get_env.c
@@ -9,12 +9,12 @@ char *_getenv(const char *name)
 {
 char **env;
 size_t name_len = _strlen(name);
-for (env = environment; *env != null; env++)
+for (env = environment; *env != NULL; env++)
 {
 if (_strncmp(*env, name, name_len) == 0 && (*env)[name_len] == '=')
 {
 return (&(*env)[name_len + 1]);
 }
 }
-return (null);
+return (NULL);
 }
