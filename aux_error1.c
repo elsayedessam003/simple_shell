d549bee46eeffd15158a5122effea583196c974c
hannaabdou <hannaabdou743@icloud.com>
2023-09-20 12:45:25 -0700

    a

--- aux_error1.c
+++ aux_error1.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * strcat_cd - function that concatenates the message for cd error
- *
  * @datash: data relevant (directory)
  * @msg: message to print
  * @error: output message
